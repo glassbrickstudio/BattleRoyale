51ea2eccdfe1d91207ca93a37ad2d07c098f400b
glassbrickstudio <55618307+glassbrickstudio@users.noreply.github.com>
2019-11-21 18:41:04 +0530

    game upload

---
+++ BattleRoyale/Assets/Tutorial/CompassScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompassScript : MonoBehaviour
+{
+
+
+    public RawImage CompassImage;
+    public Transform Player;
+    public Text CompassDirectionText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        CompassImage.uvRect = new Rect(Player.localEulerAngles.y / 360, 0, 1, 1);
+        Vector3 forward = Player.transform.forward;
+
+        forward.y = 0;
+
+        float headingAngle = Quaternion.LookRotation(forward).eulerAngles.y;
+        headingAngle = 5 * (Mathf.RoundToInt(headingAngle / 5.0f));
+
+        int displayangle;
+
+        //convert to int
+        displayangle = Mathf.RoundToInt(headingAngle);
+
+        switch (displayangle)
+        {
+            case 0:
+                CompassDirectionText.text = "N";
+                break;
+            case 360:
+                CompassDirectionText.text = "N";
+                break;
+            case 45:
+                CompassDirectionText.text = "NE";
+                break;
+            case 90:
+                CompassDirectionText.text = "E";
+                break;
+            case 130:
+                CompassDirectionText.text = "SE";
+                break;
+            case 180:
+                CompassDirectionText.text = "S";
+                break;
+            case 225:
+                CompassDirectionText.text = "SW";
+                break;
+            case 270:
+                CompassDirectionText.text = "W";
+                break;
+            default:
+                CompassDirectionText.text = headingAngle.ToString();
+                break;
+
+        }
+
+    }
+
+
+
+}
